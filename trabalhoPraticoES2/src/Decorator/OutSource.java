539bb52638536d16a0314c4602455fa55b9cb5c9
Gonçalo Marques <goncamarqs@gmail.com>
2022-04-06 09:38:39 +0100

    Bridge(?)
    Bridge, mais ou menos, mais para menos do que para mais

---
+++ trabalhoPraticoES2/src/Decorator/OutSource.java
@@ -0,0 +1,15 @@
+package Decorator;
+
+public class OutSource implements sapatoFinal{
+    private String outsource;
+    public OutSource() {
+    }
+
+    public String getOutsource() {
+        return outsource;
+    }
+
+    public void setOutsource(String outsource) {
+        this.outsource = outsource;
+    }
+}
